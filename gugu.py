418f836a9b0f87260929359299a03c9cd1af91ac
zyground <zyground@gmail.com>
2020-05-18 20:32:12 +0900

    gugu.py 수정

---
+++ gugu.py
@@ -0,0 +1,7 @@
+print('Hello world!!, This is a gugudan program2\n')
+
+for i in range(2, 10):
+    print('안녕')
+    for j in range(1, 10):
+        print('{0}*{1}={2}'.format(i, j, i*j))
+    print('\n')
